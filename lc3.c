8a850852397fce8de296df6e0a96499ad4cdc639
fosseddy <markfosseddy@gmail.com>
2022-11-07 17:24:52 +0500

    port load/store ops, add xor

--- lc3.c
+++ lc3.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <assert.h>
 
 #define MEMORY_CAP (1 << 16)
 
-typedef unsigned short word;
-typedef unsigned char byte;
+typedef unsigned short u16;
+typedef unsigned char u8;
 
 enum {
     R_R0 = 0,
@@ -30,26 +31,26 @@ enum {
 enum {
     OP_BR = 0,
     OP_ADD,
-    OP_LD,
-    OP_ST,
+    OP_LDB,
+    OP_STB,
     OP_RESERVED1,
     OP_AND,
-    OP_LDR,
-    OP_STR,
-    OP_RESERVED2,
-    OP_NOT,
-    OP_LDI,
-    OP_STI,
-    OP_JMP,
+    OP_LDW,
+    OP_STW,
+    OP_RTI, // @TODO(art): not implemented
+    OP_XOR,
     OP_RESERVED3,
+    OP_RESERVED4,
+    OP_JMP,
+    OP_SHF,
     OP_LEA,
     OP_TRAP
 };
 
-static word regs[R_COUNT] = {0};
-static byte memory[MEMORY_CAP] = {0};
+static u16 regs[R_COUNT] = {0};
+static u8 memory[MEMORY_CAP] = {0};
 
-word sext(word value, size_t bit_len)
+u16 sext(u16 value, size_t bit_len)
 {
     if (value >> (bit_len - 1) & 0x1) {
         return 0xFFFF << bit_len | value;
@@ -58,9 +59,9 @@ word sext(word value, size_t bit_len)
     return value;
 }
 
-void setcc(word value)
+void setcc(u16 value)
 {
-    word nzp;
+    u16 nzp;
     if (value == 0) {
         nzp = CC_Z;
     } else if (value >> 15) {
@@ -69,26 +70,15 @@ void setcc(word value)
         nzp = CC_P;
     }
 
-    regs[R_PSR] = regs[R_PSR] & 0x8000 | nzp;
+    regs[R_PSR] = (regs[R_PSR] & 0x8000) | nzp;
 }
 
-void debug_regs()
-{
-    for (size_t i = 0; i < 8; ++i) {
-        printf("R%lu: %5hu |", i, regs[i]);
-    }
-    printf("NZP: %hx |", regs[R_PSR] & 0x7);
-    printf("MODE: %hu |", regs[R_PSR] >> 15);
-    printf("PC: 0x%04x |", regs[R_PC]);
-    printf("\n");
-}
-
-word mem_readw(word idx)
+u16 mem_readw(u16 idx)
 {
     return memory[idx + 1] << 8 | memory[idx];
 }
 
-void mem_writew(word w, word addr)
+void mem_writew(u16 w, u16 addr)
 {
     memory[addr] = w & 0xFF;
     memory[addr + 1] = w >> 8;
@@ -96,16 +86,12 @@ void mem_writew(word w, word addr)
 
 int main(void)
 {
-    regs[R_R1] = 3;
-    regs[R_R2] = 9;
-
-    regs[R_PC] = 0;
+    regs[R_R0] = 4;
 
     size_t psz = 0;
-    word program[10] = {0};
+    u16 program[10] = {0};
 
-    program[psz++] = OP_ADD << 12 | R_R0 << 9 | R_R1 << 6 | R_R2;
-    program[psz++] = OP_ADD << 12 | R_R0 << 9 | R_R1 << 6 | 0x1 << 5 | 4 & 0x1F;
+    program[psz++] = OP_XOR << 12 | R_R0 << 9 | R_R0 << 6 | 0x1 << 5 | 0x3F;
     program[psz++] = 69;
 
     for (size_t i = 0, j = 0; i < psz; ++i, j += 2) {
@@ -113,18 +99,17 @@ int main(void)
         memory[j + 1] = program[i] >> 8;
     }
 
-    debug_regs();
-
-    word inst;
+    u16 inst;
     while ((inst = mem_readw(regs[R_PC])) != 69) {
         regs[R_PC] += 2;
-        word opcode = inst >> 12;
+        u16 opcode = inst >> 12;
         switch (opcode) {
         case OP_ADD:
-        case OP_AND: {
-            word dst = inst >> 9 & 0x7;
-            word src1 = inst >> 6 & 0x7;
-            word src2 = inst & 0x7;
+        case OP_AND:
+        case OP_XOR: {
+            u16 dst = inst >> 9 & 0x7;
+            u16 src1 = inst >> 6 & 0x7;
+            u16 src2 = inst & 0x7;
 
             if (inst >> 5 & 0x1) {
                 src2 = sext(inst & 0x1F, 5);
@@ -132,68 +117,51 @@ int main(void)
                 src2 = regs[src2];
             }
 
-            if (opcode == OP_ADD) {
-                regs[dst] = regs[src1] + src2;
-            } else {
-                regs[dst] = regs[src1] & src2;
+            switch (opcode) {
+            case OP_ADD: regs[dst] = regs[src1] + src2; break;
+            case OP_AND: regs[dst] = regs[src1] & src2; break;
+            case OP_XOR: regs[dst] = regs[src1] ^ src2; break;
+            default: assert(0 && "unreachable\n");
             }
 
             setcc(regs[dst]);
         } break;
 
-        //case OP_NOT: {
-        //    u16 dst = inst >> 9 & 0x7;
-        //    u16 src = inst >> 6 & 0x7;
-        //    regs[dst] = ~regs[src];
-        //    setcc(regs[dst]);
-        //} break;
+        case OP_STB: {
+            u16 src = inst >> 9 & 0x7;
+            u16 base = inst >> 6 & 0x7;
+            u16 boffset6 = sext(inst & 0x3F, 9);
+            u16 idx = regs[base] + boffset6;
+            memory[idx] = regs[src] & 0xFF;
+        } break;
 
-        //case OP_ST: {
-        //    u16 src = inst >> 9 & 0x7;
-        //    u16 offset9 = sext(inst & 0x1FF, 9);
-        //    memory[regs[REG_PC] + offset9] = regs[src];
-        //} break;
+        case OP_LDB: {
+            u16 dst = inst >> 9 & 0x7;
+            u16 base = inst >> 6 & 0x7;
+            u16 boffset6 = sext(inst & 0x3F, 6);
+            u16 idx = regs[base] + boffset6;
+            regs[dst] = sext(memory[idx], 8);
+            setcc(regs[dst]);
+        } break;
 
-        //case OP_STI: {
-        //    u16 src = inst >> 9 & 0x7;
-        //    u16 offset9 = sext(inst & 0x1FF, 9);
-        //    u16 idx = regs[REG_PC] + offset9;
-        //    u16 addr = memory[idx];
-        //    memory[addr] = regs[src];
-        //} break;
+        case OP_STW: {
+            u16 src = inst >> 9 & 0x7;
+            u16 base = inst >> 6 & 0x7;
+            u16 offset6 = sext(inst & 0x3F, 6) << 1;
+            u16 idx = regs[base] + offset6;
+            mem_writew(regs[src], idx);
+            // @TODO(art): check regs[base] alignment
+        } break;
 
-        //case OP_STR: {
-        //    u16 src = inst >> 9 & 0x7;
-        //    u16 base = inst >> 6 & 0x7;
-        //    u16 offset6 = sext(inst & 0x3F, 6);
-        //    u16 idx = regs[base] + offset6;
-        //    memory[idx] = regs[src];
-        //} break;
-
-        //case OP_LD: {
-        //    u16 dst = inst >> 9 & 0x7;
-        //    u16 offset9 = sext(inst & 0x1FF, 9);
-        //    regs[dst] = memory[regs[REG_PC] + offset9];
-        //    setcc(regs[dst]);
-        //} break;
-
-        //case OP_LDI: {
-        //    u16 dst = inst >> 9 & 0x7;
-        //    u16 offset9 = sext(inst & 0x1FF, 9);
-        //    u16 idx = regs[REG_PC] + offset9;
-        //    u16 addr = memory[idx];
-        //    regs[dst] = memory[addr];
-        //    setcc(regs[dst]);
-        //} break;
-
-        //case OP_LDR: {
-        //    u16 dst = inst >> 9 & 0x7;
-        //    u16 base = inst >> 6 & 0x7;
-        //    u16 offset6 = sext(inst & 0x3F, 6);
-        //    u16 idx = regs[base] + offset6;
-        //    regs[dst] = memory[idx];
-        //    setcc(regs[dst]);
-        //} break;
+        case OP_LDW: {
+            u16 dst = inst >> 9 & 0x7;
+            u16 base = inst >> 6 & 0x7;
+            u16 offset6 = sext(inst & 0x3F, 6) << 1;
+            u16 idx = regs[base] + offset6;
+            regs[dst] = mem_readw(idx);
+            setcc(regs[dst]);
+            // @TODO(art): check regs[base] alignment
+        } break;
 
         //case OP_LEA: {
         //    u16 dst = inst >> 9 & 0x7;
@@ -211,10 +179,11 @@ int main(void)
         //    }
         //} break;
 
-        //case OP_JMP: {
-        //    u16 base = inst >> 6 & 0x7;
-        //    regs[REG_PC] += regs[base];
-        //} break;
+        case OP_JMP: {
+            u16 base = inst >> 6 & 0x7;
+            regs[R_PC] = regs[base];
+            // @TODO(art): check regs[base] alignment
+        } break;
 
         //case OP_TRAP: {
         //    u16 trapvec8 = inst & 0xFF;
@@ -229,8 +198,6 @@ int main(void)
         //default:
         //    fprintf(stderr, "opcode %4x not implemented\n", opcode);
         }
-
-        debug_regs();
     }
 
     return 0;
