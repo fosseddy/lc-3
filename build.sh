b9fdaee39603293c07167bcdde4d32ee6a1e1a56
fosseddy <markfosseddy@gmail.com>
2022-11-13 21:45:52 +0500

    build for asm

--- build.sh
+++ build.sh
@@ -2,7 +2,25 @@
 
 set -xe
 
-FLAGS="-g -Wall -Wextra -std=c11 -pedantic"
-#FLAGS="-g -Wextra -std=c11 -pedantic"
+if [ "$1" = "clean" ]; then
+    rm asm lc3
+    exit 0
+fi
 
-gcc $FLAGS -o lc3 lc3.c
+FLAGS_PROD="-g -Wall -Wextra -std=c11 -pedantic"
+FLAGS_DEF="-g -Wextra -std=c11 -pedantic"
+
+FLAGS=$FLAGS_DEF
+
+if [ "$1" = "prod" ] || [ "$2" = "prod" ]; then
+    FLAGS=$FLAGS_PROD
+fi
+
+if [ "$1" = "lc3" ]; then
+    gcc $FLAGS -o lc3 lc3.c
+elif [ "$1" = "asm" ]; then
+    gcc $FLAGS -o asm asm.c
+else
+    gcc $FLAGS -o lc3 lc3.c &
+    gcc $FLAGS -o asm asm.c
+fi
