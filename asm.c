e3957a659e6796a49444a8227c20655b2dfebafa
fosseddy <markfosseddy@gmail.com>
2022-11-14 23:26:53 +0500

    lex registers

--- asm.c
+++ asm.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <assert.h>
 
 char *read_file(char *path)
 {
@@ -48,9 +49,18 @@ enum token_kind {
     T_COMMA,
     T_NEWLINE,
 
+    T_LABEL,
+
     T_ADD,
 
     T_R0,
+    T_R1,
+    T_R2,
+    T_R3,
+    T_R4,
+    T_R5,
+    T_R6,
+    T_R7,
 
     T_DECIMAL
 };
@@ -89,6 +99,28 @@ int next(struct scanner *s, char c)
     return peek(s) == c;
 }
 
+void make_token(struct token *t, enum token_kind kind, struct scanner *s)
+{
+    t->kind = kind;
+    t->start = s->start;
+    t->len = s->curr - s->start;
+    t->line = s->line;
+}
+
+void make_newline_token(struct token *t, struct scanner *s)
+{
+    t->kind = T_NEWLINE;
+    t->start = "\\n";
+    t->len = 2;
+    t->line = s->line;
+}
+
+int check_kwd(struct scanner *s, size_t start, size_t len, char *rest)
+{
+    return (size_t) (s->curr - s->start) == start + len &&
+           memcmp(s->start + start, rest, len) == 0;
+}
+
 int main(void)
 {
     // @LEAK(art): let OS free it for now
@@ -103,8 +135,8 @@ int main(void)
     };
 
     // @TODO(art): make growable array
-    size_t idx = 0;
     struct token tokens[50] = {0};
+    size_t tokens_len = 0;
 
     for (;;) {
         s.start = s.curr;
@@ -118,15 +150,45 @@ int main(void)
             while (isalnum(peek(&s))) advance(&s);
             switch (*s.start) {
             case 'a':
-                if (s.curr - s.start == 3 &&
-                        memcmp(s.start + 1, "dd", 2) == 0) {
-                    printf("keyword: add\n");
+                if (check_kwd(&s, 1, 2, "dd")) {
+                    make_token(&tokens[tokens_len++], T_ADD, &s);
+                    continue;
                 }
                 break;
 
-            default: printf("label: %.*s\n", s.curr - s.start, s.start);
+            case 'r':
+                if (s.curr - s.start == 2) {
+                    switch (*(s.start + 1)) {
+                    case '0':
+                        make_token(&tokens[tokens_len++], T_R0, &s);
+                        continue;
+                    case '1':
+                        make_token(&tokens[tokens_len++], T_R1, &s);
+                        continue;
+                    case '2':
+                        make_token(&tokens[tokens_len++], T_R2, &s);
+                        continue;
+                    case '3':
+                        make_token(&tokens[tokens_len++], T_R3, &s);
+                        continue;
+                    case '4':
+                        make_token(&tokens[tokens_len++], T_R4, &s);
+                        continue;
+                    case '5':
+                        make_token(&tokens[tokens_len++], T_R5, &s);
+                        continue;
+                    case '6':
+                        make_token(&tokens[tokens_len++], T_R6, &s);
+                        continue;
+                    case '7':
+                        make_token(&tokens[tokens_len++], T_R7, &s);
+                        continue;
+                    }
+                }
+                break;
             }
 
+            make_token(&tokens[tokens_len++], T_LABEL, &s);
             continue;
         }
 
@@ -137,12 +199,7 @@ int main(void)
             break;
 
         case '\n':
-            tokens[idx++] = (struct token) {
-                .kind = T_NEWLINE,
-                .len = 2,
-                .start = "\\n",
-                .line = s.line
-            };
+            make_newline_token(&tokens[tokens_len++], &s);
             s.line += 1;
             break;
 
@@ -150,11 +207,26 @@ int main(void)
             while (!next(&s, '\n') && has_chars(&s)) advance(&s);
             break;
 
+        case ',':
+            make_token(&tokens[tokens_len++], T_COMMA, &s);
+            break;
+
+        case '#':
+            s.start++;
+
+            if (next(&s, '-')) advance(&s);
+            while (isdigit(peek(&s))) advance(&s);
+
+            // @TODO(art): handle error
+            assert(s.curr - s.start > 0);
+            make_token(&tokens[tokens_len++], T_DECIMAL, &s);
+            break;
+
         default: printf("Unknown char: '%c'\n", c);
         }
     }
 
-    for (size_t i = 0; i < idx; ++i) {
+    for (size_t i = 0; i < tokens_len; ++i) {
         printf("line: %lu ", tokens[i].line);
         printf("kind: %i ", tokens[i].kind);
         printf("len: %lu ", tokens[i].len);
